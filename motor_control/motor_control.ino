9e86d4cbaec57d072d71a649caa261cee93675cd
Dan <jakq277@berkeley.edu>
2019-09-19 14:24:02 -0700

    Revert "functions of 4 motors implemented"
    This reverts commit 8de60a966316a9838c1e04fc857c8d0d2e85f29b.

--- motor_control/motor_control.ino
+++ motor_control/motor_control.ino
@@ -4,22 +4,18 @@
 
 // *** CONSTANTS ***
 const int BIG_SERVO_PIN = 9;
-const int SMALL_SERVO_PIN = 6; 
 const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution for motor
 const int rolePerMinute = 15;
 
-
-
 // *** OBJECT INITIALIZATION ***
 Stepper myStepper_1(stepsPerRevolution, 10, 12, 11, 13);
-Stepper myStepper_2(stepsPerRevolution, 2, 4, 3, 5); 
+// Stepper myStepper_2(stepsPerRevolution, , , , );
 Servo bigServo;
-Servo smallServo;
+//Servo smallServo;
 
 
 // *** VARIABLES ***
 int bigServoPos = 90;
-int smallServoPos = 90;
 int pos = 0;
 int stepCount = 0;  // number of steps the motor has taken
 
@@ -27,7 +23,7 @@ int stepCount = 0;  // number of steps the motor has taken
 
 // ***FUNCTIONS***
 
-// **BIG SERVO**
+// BIG SERVO
 void turnBigServoLeft() {
   if(bigServo.read() == 0)
     return;
@@ -39,39 +35,20 @@ void turnBigServoLeft() {
 }
   
 void turnBigServoRight() {
-  if(bigServo.read() == 180)
+  if(myServo.read() == 180)
     return;
     
   else{
     bigServoPos += 10;
-    bigServo.write(bigServoPos);
+    myServo.write(bigServoPos);
   }
   
 }
 
-// **SMALL SERVO**
-void turnSmallServoLeft() {
-  if(smallServo.read() == 0)
-    return;
-  else{
-    smallServoPos -= 10;
-    smallServo.write(smallServoPos);
-    }
-    
-}
-  
-void turnSmallServoRight() {
-  if(smallServo.read() == 180)
-    return;
-    
-  else{
-    smallServoPos += 10;
-    smallServo.write(smallServoPos);
-  }
-  
-}
+// SMALL SERVO
 
-// **STEPPER_1**
+
+// STEPPER_1
 void turnStepper1_left() {
   myStepper_1.step(-stepsPerRevolution);
 }
@@ -80,14 +57,8 @@ void turnStepper1_right() {
   myStepper_1.step(stepsPerRevolution);
 }
 
-// **STEPPER_2**
-void turnStepper2_left() {
-  myStepper_2.step(-stepsPerRevolution);
-}
+// STEPPER_2
 
-void turnStepper2_right() {
-  myStepper_2.step(stepsPerRevolution);
-}
 
 
 
@@ -95,10 +66,7 @@ void setup() {
   // nothing to do inside the setup
     bigServo.attach(BIG_SERVO_PIN);
     bigServo.write(0);
-    smallServo.attach(SMALL_SERVO_PIN);
-    smallServo.write(0);
     myStepper_1.setSpeed(rolePerMinute);
-    myStepper_2.setSpeed(rolePerMinute);
 }
 
 
