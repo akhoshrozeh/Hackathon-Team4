7480b04d7dbb4b69f12315da24e72733e8ab856e
Anthony Khoshrozeh <akhoshrozeh@gmail.com>
2019-09-19 19:15:34 -0700

    code for testing bluetooth cxn added

--- motor_control/motor_control.ino
+++ motor_control/motor_control.ino
@@ -9,7 +9,8 @@ const int BIG_SERVO_PIN = 9;
 const int SMALL_SERVO_PIN = 6; 
 const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution for motor
 const int rolePerMinute = 15;
-
+#define RX_PIN 0
+#define TX_PIN 1
 
 
 // *** OBJECT INITIALIZATION ***
@@ -17,7 +18,7 @@ Stepper myStepper_1(stepsPerRevolution, 10, 12, 11, 13);
 Stepper myStepper_2(stepsPerRevolution, 2, 4, 3, 5); 
 Servo bigServo;
 Servo smallServo;
-
+SoftwareSerial HM10 = SoftwareSerial(0, 1);
 
 // *** VARIABLES ***
 int bigServoPos = 90;
@@ -92,8 +93,15 @@ void turnStepper2_right() {
 }
 
 
+
+
+   
 // *** SETUP ***
 void setup() {
+
+    Serial.begin(9600);
+    HM10.begin(9600);
+    
     bigServo.attach(BIG_SERVO_PIN);
     bigServo.write(0);
     smallServo.attach(SMALL_SERVO_PIN);
@@ -101,9 +109,11 @@ void setup() {
     myStepper_1.setSpeed(rolePerMinute);
     myStepper_2.setSpeed(rolePerMinute);
 
-    Serial.begin(9600);
-    SoftwareSerial HM10 (0,1);  
-    HM10.begin(9600);
+   
+   // pinMode(0, INPUT);
+   // pinMode(1, OUTPUT);
+    
+    
 }
 
 // GUIDE FOR INPUT 
@@ -118,18 +128,30 @@ void setup() {
 
 void loop() {
     HM10.listen();
-    while(HM10.available) {
+    while(HM10.available() > 0) {
       byte data = HM10.read();
 
       if(data == 1)
           Serial.println("hand up");
       else if(data == 2)
           Serial.println("hand down");
+      else if(data == 3)
+          Serial.println("elbow up");
+      else if(data == 4)
+          Serial.println("elbow down");
+      else if(data == 5)
+          Serial.println("wrist ccw");
+      else if(data == 6)
+          Serial.println("wrist cw");
+      else if(data == 7)
+          Serial.println("arm left");
+      else if(data == 8)
+          Serial.println("arm right");
 
-
-      
-      
-    
+       else {
+        // do nothing 
+        }
+          
     
     }
    
